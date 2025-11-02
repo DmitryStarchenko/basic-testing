058b341acbca2d48dd6e1c2a0cd6701063318e50
Dmitry <Dololob92@gmail.com>
2025-11-02 14:48:53 +0300

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,80 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 5,
+      b: 3,
+      action: Action.Add,
+    });
+    expect(result).toBe(8);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 10,
+      b: 4,
+      action: Action.Subtract,
+    });
+    expect(result).toBe(6);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 7,
+      b: 2,
+      action: Action.Multiply,
+    });
+    expect(result).toBe(14);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 15,
+      b: 3,
+      action: Action.Divide,
+    });
+    expect(result).toBe(5);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 2,
+      b: 3,
+      action: Action.Exponentiate,
+    });
+    expect(result).toBe(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 5,
+      b: 3,
+      action: '%',
+    });
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const result1 = simpleCalculator({
+      a: '5',
+      b: 3,
+      action: Action.Add,
+    });
+    expect(result1).toBeNull();
+
+    const result2 = simpleCalculator({
+      a: true,
+      b: false,
+      action: Action.Add,
+    });
+    expect(result2).toBeNull();
+
+    const result3 = simpleCalculator({
+      a: [1, 2],
+      b: 3,
+      action: Action.Add,
+    });
+    expect(result3).toBeNull();
   });
 });
